703e2196ddd27d071ffac323fdb26b12bef03e90
Francisco de Maussion <franciscodemaussion@gmail.com>
2020-05-12 12:35:47 -0300

    Repair Imports

--- src/main/java/com/thebadtouch/searchengine/services/indexing/impl/IndexingServiceImpl.java
+++ src/main/java/com/thebadtouch/searchengine/services/indexing/impl/IndexingServiceImpl.java
@@ -9,7 +9,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
-import sun.rmi.runtime.Log;
 
 import java.util.*;
 
